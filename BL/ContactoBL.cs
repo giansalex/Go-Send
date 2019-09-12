345d5f4d325463fa4cc795c2781564509a958766
Giancarlos Salas <giansalex@gmail.com>
2019-09-11 21:23:03 -0500

    add fondo

---
+++ BL/ContactoBL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoSend.DB.SQLite;
+namespace GoSend.DB.BL
+{
+    public class ContactoBL
+    {
+        /// <summary>
+        /// Añade un contacto a la lista
+        /// </summary>
+        /// <param name="contacto">Nuevo contacto</param>
+        public void addContact(Contacto contacto)
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                dbContext.AddObject(contacto);                
+            }
+        }
+
+        /// <summary>
+        /// Obtiene una lista de todos los Contactos
+        /// </summary>
+        /// <returns></returns>
+        public List<Contacto> getAllContacts()
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                return dbContext.contacto.ToList();
+            }
+        }
+
+        /// <summary>
+        /// Obtiene un contacto por su ID
+        /// </summary>
+        /// <param name="idContacto">Id del contacto</param>
+        /// <returns></returns>
+        public Contacto getContact(int idContacto)
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                var contact = (from c in dbContext.contacto
+                               where c.id == idContacto
+                                select c).FirstOrDefault();
+                return contact;
+            }
+        }
+    }
+}
---
+++ BL/MensajeBL.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoSend.DB.SQLite;
+
+namespace GoSend.DB.BL
+{
+    public class MensajeBL
+    {
+        /// <summary>
+        /// Añade un mensaje
+        /// </summary>
+        /// <param name="message">New Message</param>
+        public void addMessage(Mensaje message)
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                dbContext.AddObject(message);
+            }
+        }
+
+        public void deleteMessage(int idMessage)
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                var objEntity = (from n in dbContext.mensaje
+                                 where n.id == idMessage
+                                 select n).FirstOrDefault(); 
+                dbContext.RemoveObject(objEntity);
+            }
+        }
+
+        /// <summary>
+        /// Obtiene mensaje por su id
+        /// </summary>
+        /// <param name="idMessage">Id del mensaje</param>
+        /// <returns></returns>
+        public Mensaje getMessage(int idMessage)
+        {
+            using (var dbContext = new GoSendEntities())
+            {
+                var msg = (from n in dbContext.mensaje
+                           where n.id == idMessage
+                           select n).FirstOrDefault();
+                return msg;
+            }
+        }
+    }
+}
